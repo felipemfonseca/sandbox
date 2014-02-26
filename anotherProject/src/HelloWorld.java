1534c9680305254f73bcc11a8a602a09ea5ca9db
Intel <Intel@Intel-PC.Home>
2014-02-26 01:37:39 -0300

    Changes!!!

--- anotherProject/src/HelloWorld.java
+++ anotherProject/src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 	
 	public static void main(String[] args){
-		System.out.println("You again?");
+		System.out.println("You again? Im making some serious changes....");
 	}
 
 }
